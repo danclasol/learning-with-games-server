d7b7b6450134de541b74801e172b62d4f41a7b0f
Daniel <danielclark.projects@gmail.com>
2023-04-05 18:00:58 +0200

    Var enviroment settings

--- src/server.js
+++ src/server.js
@@ -3,13 +3,17 @@ import '#Config/env.js';
 import httpServer from '#Config/http.js';
 
 const PORT = process.env.PORT || 8080;
-const connectionString = process.env.MONGODB_URI_DEV;
+const environment = process.env.NODE_ENV || 'development';
+const connectionString =
+	environment === 'production'
+		? process.env.MONGODB_URI_PRO
+		: process.env.MONGODB_URI_DEV;
 
 const bootstrap = async () => {
 	await connectDB(connectionString);
 
 	httpServer.listen(PORT, () => {
-		console.log(`Server running on port ${PORT}`);
+		console.log(`Server running on port ${PORT} (${environment})`);
 	});
 };
 
